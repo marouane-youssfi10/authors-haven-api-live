e0350bd3527679e6e9f3beebc59f9aae64ae7f43
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-04-14 02:44:48 +0000

    chore : setup dokcer with compose

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -7,10 +7,17 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env(
     'DJANGO_SECRET_KEY',
-    default="VoQE5G6ZQGRFR345NSQSDOL325cds23nZEC23412dfsdd43QSDF26NS2352", 
+    default="VoQE5G62Qu1Sk8cmBMa8V8D4nYhWazjaEoH9p9wWGPF4Pv23A3M68Wtme2BpHSwt", 
 )
 
 
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 # django-insecure-*=g)&&92^k7zze3m+^4d4e-us-l_98t*rg-b5oah$-ho$-n#+^
+
+# EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+# EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+# EMAIL_PORT = env("EMAIL_PORT")
+# DEFAULT_FROM_EMAIL = ""
+# DOMAIN = env("DOMAIN")
+# SITE_NAME = "Authors Haven"
